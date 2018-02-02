d8e2d50c5cda055bb447c24e6d4a61c1b6418ae5
Russell Matney <russell.matney@gmail.com>
2018-02-02 12:33:48 -0500

    feat(GraphQL): updates interpret funcs to use and apply error constraint
    Also catches and converts errors thrown in resolve functions.

--- src/GraphQL.hs
+++ src/GraphQL.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 -- | Interface for GraphQL API.
 --
 -- __Note__: This module is highly subject to change. We're still figuring
@@ -30,7 +31,7 @@ import qualified Data.List.NonEmpty as NonEmpty
 import GraphQL.API (HasObjectDefinition(..))
 import GraphQL.Internal.Execution
   ( VariableValues
-  , ExecutionError
+  , ExecutionError(..)
   , substituteVariables
   )
 import qualified GraphQL.Internal.Execution as Execution
@@ -51,7 +52,12 @@ import GraphQL.Internal.Output
   )
 import GraphQL.Internal.Schema (Schema)
 import qualified GraphQL.Internal.Schema as Schema
-import GraphQL.Resolver (HasResolver(..), Result(..))
+import GraphQL.Resolver
+  ( HasResolver(..)
+  , Result(..)
+  , AsGraphQLError(..)
+  , throwE
+  )
 import GraphQL.Value (Name, NameError, Value, pattern ValueObject)
 
 -- | Errors that can happen while processing a query document.
@@ -85,7 +91,7 @@ instance GraphQLError QueryError where
 
 -- | Execute a GraphQL query.
 executeQuery
-  :: forall api m. (HasResolver m api, Applicative m, HasObjectDefinition api)
+  :: forall api e m. (Applicative m, HasResolver e m api, HasObjectDefinition api)
   => Handler m api -- ^ Handler for the query. This links the query to the code you've written to handle it.
   -> QueryDocument VariableValue  -- ^ A validated query document. Build one with 'compileQuery'.
   -> Maybe Name -- ^ An optional name. If 'Nothing', then executes the only operation in the query. If @Just "something"@, executes the query named @"something".
@@ -94,9 +100,17 @@ executeQuery
 executeQuery handler document name variables =
   case getOperation document name variables of
     Left e -> pure (ExecutionFailure (singleError e))
-    Right operation -> toResult <$> resolve @m @api handler (Just operation)
+    Right operation -> do
+      result <- performOperation operation
+      pure $ toResponse result
   where
-    toResult (Result errors result) =
+    performOperation :: SelectionSetByType Value -> m (Result Value)
+    performOperation operation =
+      resolve @e @m @api handler (Just operation) `catchError` returnError
+
+
+    toResponse :: Result Value -> Response
+    toResponse (Result errors result) =
       case result of
         -- TODO: Prevent this at compile time. Particularly frustrating since
         -- we *know* that api has an object definition.
@@ -106,34 +120,38 @@ executeQuery handler document name variables =
             Just errs -> PartialSuccess object (map toError errs)
         v -> ExecutionFailure (singleError (NonObjectResult v))
 
+    returnError = throwE . asGraphQLError
+
 -- | Create a GraphQL schema.
 makeSchema :: forall api. HasObjectDefinition api => Either QueryError Schema
 makeSchema = first SchemaError (Schema.makeSchema <$> getDefinition @api)
+
 
 -- | Interpet a GraphQL query.
 --
 -- Compiles then executes a GraphQL query.
 interpretQuery
-  :: forall api m. (Applicative m, HasResolver m api, HasObjectDefinition api)
+  :: forall api e m. (Applicative m, HasResolver e m api, HasObjectDefinition api)
   => Handler m api -- ^ Handler for the query. This links the query to the code you've written to handle it.
   -> Text -- ^ The text of a query document. Will be parsed and then executed.
   -> Maybe Name -- ^ An optional name for the operation within document to run. If 'Nothing', execute the only operation in the document. If @Just "something"@, execute the query or mutation named @"something"@.
   -> VariableValues -- ^ Values for variables defined in the query document. A map of 'Variable' to 'Value'.
   -> m Response -- ^ The outcome of running the query.
-interpretQuery handler query name variables =
-  case makeSchema @api >>= flip compileQuery query of
-    Left err -> pure (PreExecutionFailure (toError err :| []))
-    Right document -> executeQuery @api @m handler document name variables
+interpretQuery handler query name variables = do
+    case makeSchema @api >>= flip compileQuery query of
+         Left err -> pure (PreExecutionFailure (toError err :| []))
+         Right document -> executeQuery @api @e @m handler document name variables
+
 
 -- | Interpret an anonymous GraphQL query.
 --
 -- Anonymous queries have no name and take no variables.
 interpretAnonymousQuery
-  :: forall api m. (Applicative m, HasResolver m api, HasObjectDefinition api)
+  :: forall api e m. (Applicative m, HasResolver e m api, HasObjectDefinition api)
   => Handler m api -- ^ Handler for the anonymous query.
   -> Text -- ^ The text of the anonymous query. Should defined only a single, unnamed query operation.
   -> m Response -- ^ The result of running the query.
-interpretAnonymousQuery handler query = interpretQuery @api @m handler query Nothing mempty
+interpretAnonymousQuery handler query = interpretQuery @api @e @m handler query Nothing mempty
 
 -- | Turn some text into a valid query document.
 compileQuery :: Schema -> Text -> Either QueryError (QueryDocument VariableValue)
